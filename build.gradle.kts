ccd344ef5212998c9c9e760ef0ad003fda59f43b
aladinsws <aladinsws@gmail.com>
2026-06-02 17:19:43 +0200

    remove unused signing and publishing configurations

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,16 +39,6 @@ intellijPlatform {
         }
     }
 
-    signing {
-        certificateChain = providers.environmentVariable("CERTIFICATE_CHAIN")
-        privateKey = providers.environmentVariable("PRIVATE_KEY")
-        password = providers.environmentVariable("PRIVATE_KEY_PASSWORD")
-    }
-
-    publishing {
-        token = providers.environmentVariable("PUBLISH_TOKEN")
-    }
-
     pluginVerification {
         ides {
             recommended()
