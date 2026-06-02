82cdeb50fb614d0232e5a7fc0cc3ae45dba36c08
GitHub Action <action@github.com>
2026-06-02 11:04:23 +0000

    Template cleanup

--- src/main/kotlin/com/github/aladinsws/mybatislogformatsql/services/MyProjectService.kt
+++ src/main/kotlin/com/github/aladinsws/mybatislogformatsql/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.aladinsws.mybatislogformatsql.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.aladinsws.mybatislogformatsql.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
